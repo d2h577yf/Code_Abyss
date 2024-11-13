a59aa2a0e84e0599b042048483c30a140a04563b
d2h577yf <why20051028XY@icloud.com>
2024-11-13 15:25:32 +0800

    d2h5 2024/11/13

--- XDOJ/Starfish/ZiFuChuan/85.c
+++ XDOJ/Starfish/ZiFuChuan/85.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-    char S1[101],S2[101];
-    int j,cnt = 0,max = 0;
-    double similarity;
-    memset(S1,'\0',sizeof(S1));
-    memset(S2,'\0',sizeof(S2));
-    scanf("%[^\n]",S1);
-    getchar();
-    scanf("%[^\n]",S2);
+int main() {
+  char S1[101], S2[101];
+  int j, cnt = 0, max = 0;
+  double similarity;
+  memset(S1, '\0', sizeof(S1));
+  memset(S2, '\0', sizeof(S2));
+  scanf("%[^\n]", S1);
+  getchar();
+  scanf("%[^\n]", S2);
 
-    for(int i = 0;S1[i] != 0;i++){
-        if(S1[i] >= 'a'){
-            S1[i] -= 32;
-        }
+  for (int i = 0; S1[i] != 0; i++) {
+    if (S1[i] >= 'a') {
+      S1[i] -= 32;
     }
-    for(int i = 0;S2[i] != 0;i++){
-        if(S2[i] >= 'a'){
-            S2[i] -= 32;
-        }
+  }
+  for (int i = 0; S2[i] != 0; i++) {
+    if (S2[i] >= 'a') {
+      S2[i] -= 32;
     }
-    for(j = 0;S1[j] != 0;j++){
-        for(int k = 0;S2[k] != 0;k++){
-            cnt = 0;
-            if(S1[j] == S2[k]){
-                cnt++;
-                for(int l = 1;S1[j + l] != 0 && S2[k + l] != 0;l++){
-                    if(S1[j + l] == S2[k + l]){
-                        cnt++;
-                    }
-                }
-                if(max < cnt){
-                    max = cnt;
-                }
-            }
+  }
+  for (j = 0; S1[j] != 0; j++) {
+    for (int k = 0; S2[k] != 0; k++) {
+      cnt = 0;
+      if (S1[j] == S2[k]) {
+        cnt++;
+        for (int l = 1; S1[j + l] != 0 && S2[k + l] != 0; l++) {
+          if (S1[j + l] == S2[k + l]) {
+            cnt++;
+          }
         }
+        if (max < cnt) {
+          max = cnt;
+        }
+      }
     }
+  }
 
-        similarity = 2.0 * (double)max / (double)(strlen(S1) + strlen(S2));
+  similarity = 2.0 * (double)max / (double)(strlen(S1) + strlen(S2));
 
-    printf("%.3f\n",similarity);
+  printf("%.3f\n", similarity);
 
-    return 0;
+  return 0;
 }
