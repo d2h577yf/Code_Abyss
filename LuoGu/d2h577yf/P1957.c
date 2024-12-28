962f1a106532c6c3a33fc2ae4753271823a9da4c
d2h577yf <why20051028XY@icloud.com>
2024-12-28 16:14:41 +0800

    d2h5 2024/12/28
    d2h5 2024/12/28

--- LuoGu/d2h577yf/P1957.c
+++ LuoGu/d2h577yf/P1957.c
@@ -1,10 +1,33 @@
-#include <stdbool.h>
+#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  int n;
+int main() {
+  char a;
+  int n, c, d;
+  char s[100], b[13];
+
   scanf("%d", &n);
+  getchar();
+  for (int i = 0; i < n; i++) {
+    fgets(b, 13, stdin);
 
-  return EXIT_SUCCESS;
+    if (isalpha(b[0])) {
+      sscanf(b, "%c %d %d", &a, &c, &d);
+    } else {
+      sscanf(b, "%d %d", &c, &d);
+    }
+    memset(s, 0, sizeof(s));
+
+    if (a == 'a')
+      sprintf(s, "%d+%d=%d", c, d, c + d);
+    else if (a == 'b')
+      sprintf(s, "%d-%d=%d", c, d, c - d);
+    else if (a == 'c')
+      sprintf(s, "%d*%d=%d", c, d, c * d);
+
+    printf("%s\n%d\n", s, (int)strlen(s));
+  }
+
+  return 0;
 }
