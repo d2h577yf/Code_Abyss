7534677f668dfb2f0efd2b31ccf8daf0e7bca5db
d2h577yf <why20051028XY@icloud.com>
2025-01-05 08:04:15 +0800

    d2h5 2025/1/5

---
+++ LuoGu/d2h577yf/P1042.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int **arr = malloc(2501 * sizeof(char *));
+  for (int i = 0; i < 2501; i++) {
+    *arr = malloc(26 * sizeof(char));
+  }
+
+  for (int i = 0; i < ; inc - expression) {
+  }
+
+  return EXIT_SUCCESS;
+}
